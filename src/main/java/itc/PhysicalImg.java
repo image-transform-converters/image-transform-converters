fd0da570aab23cb115e789c8ff3ddd6ad08705b8
John Bogovic <bogovicj@janelia.hhmi.org>
2019-01-20 09:36:41 -0500

    rename PhysicalImg, add doc and generic param

---
+++ src/main/java/itc/PhysicalImg.java
@@ -0,0 +1,33 @@
+package itc;
+
+import net.imglib2.RealInterval;
+import net.imglib2.RealRandomAccessible;
+
+/**
+ * A continuous image in a physical coordinate system.
+ * 
+ * Provides an image as a {@link RealRandomAccessible}, and spatial extents as a {@link RealInterval}, 
+ * in specified physical units.
+ *
+ */
+public class PhysicalImg<T>
+{
+	public static final String MICROMETER = "micrometer";
+
+	public final RealRandomAccessible<T> img;
+	public final RealInterval interval;
+	public final String unit;
+
+	public PhysicalImg( RealRandomAccessible<T> img, RealInterval interval )
+	{
+		this( img, interval, MICROMETER);
+	}
+
+	public PhysicalImg( RealRandomAccessible<T> img, RealInterval interval, String unit )
+	{
+		this.img = img;
+		this.interval = interval;
+		this.unit = unit;
+	}
+
+}
