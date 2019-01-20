51a2947a6b2a0bc68a9b80fc2ff874a31f88035a
Christian Tischer <christian.tischer@embl.de>
2019-01-20 07:21:38 +0100

    Add input and output type to interface ideas

--- src/main/java/itc/converterinterfaces/ImageTransformConverterA.java
+++ src/main/java/itc/converterinterfaces/ImageTransformConverterA.java
@@ -3,4 +3,8 @@ package itc.converterinterfaces;
 public interface ImageTransformConverterA< I, O >
 {
 	O convert( I input );
+
+	Class inputType();
+
+	Class outputType();
 }
--- src/main/java/itc/converterinterfaces/ImageTransformConverterB.java
+++ src/main/java/itc/converterinterfaces/ImageTransformConverterB.java
@@ -3,4 +3,8 @@ package itc.converterinterfaces;
 public interface ImageTransformConverterB< I, O >
 {
 	void convert( I input, O output );
+
+	Class inputType();
+
+	Class outputType(); 
 }
