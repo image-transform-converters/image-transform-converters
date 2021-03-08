92dd6cb072dcf23424a893ab5cb3025cc01b1781
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-03-08 16:34:44 +0100

    fixes licenses before release

--- src/test/java/itc/examples/ElastixLoadAffine2DFromFileAndConvertToAffineTransform2D.java
+++ src/test/java/itc/examples/ElastixLoadAffine2DFromFileAndConvertToAffineTransform2D.java
@@ -2,17 +2,17 @@
  * #%L
  * image-transform-converters
  * %%
- * Copyright (C) 2019 - 2020 John Bogovic, Nicolas Chiaruttini, and Christian Tischer
+ * Copyright (C) 2019 - 2021 John Bogovic, Nicolas Chiaruttini, and Christian Tischer
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
