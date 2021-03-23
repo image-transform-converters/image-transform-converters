a16ac2d5728ff1c7875690d2b6132511e113b7b5
Kimberly Meechan <kimberly.meechan@embl.de>
2021-03-23 16:51:54 +0000

    get rotation matrix from 3d similarity parameters

--- src/main/java/itc/transforms/elastix/ElastixSimilarityTransform3D.java
+++ src/main/java/itc/transforms/elastix/ElastixSimilarityTransform3D.java
@@ -28,5 +28,75 @@
  */
 package itc.transforms.elastix;
 
+import static java.lang.Math.sqrt;
+import static net.imglib2.util.LinAlgHelpers.quaternionToR;
+
 public class ElastixSimilarityTransform3D extends ElastixSimilarityTransform {
+
+    private double getNorm( double[] vector )
+    {
+        double norm = 0;
+        for ( int i=0; i<vector.length; i++ ) {
+            norm += vector[i]*vector[i];
+        }
+
+        if (norm > 0)
+        {
+            norm = sqrt(norm);
+        }
+
+        return norm;
+    }
+
+    public double[][] getRotationMatrix()
+    {
+        // transform parameters only gives the first 3 parts (x, y, z) of the unit quaternion - we must calculate
+        // the scalar part (w) from this
+        // based on elastix / itk source code:
+        // https://github.com/SuperElastix/elastix/blob/c3a254562d6801f1a34a2de0ec324c10632b5884/Common/Transforms/itkAdvancedSimilarity3DTransform.hxx#L126
+        // https://github.com/InsightSoftwareConsortium/ITK/blob/master/Modules/Core/Common/include/itkVersor.hxx#L444
+        // https://itk.org/Doxygen/html/classitk_1_1Versor.html#a72a0cf968370f95dac5bac84a01f93a0
+
+        double[] axis = new double[3];
+        axis[0] = TransformParameters[0];
+        axis[1] = TransformParameters[1];
+        axis[2] = TransformParameters[2];
+        double norm = getNorm( axis );
+
+        double epsilon = 1e-10;
+        if (norm >= 1.0 - epsilon)
+        {
+            for ( int i=0; i<axis.length; i++ ) {
+                axis[i] = axis[i] / (norm + epsilon * norm);
+            }
+        }
+
+        double sinangle2 = getNorm( axis );
+        double cosangle2 = sqrt( 1 - sinangle2 * sinangle2);
+
+        double[] unitQuaternion = new double[]{ axis[0], axis[1], axis[2], cosangle2 };
+
+        final double[][] matrix = new double[3][3];
+        quaternionToR( unitQuaternion, matrix );
+
+        return matrix;
+    }
+
+    public double getScalingFactor()
+    {
+        final double scalingFactor = TransformParameters[6];
+        return scalingFactor;
+    }
+
+    public double[] getTranslationInMillimeters()
+    {
+        final double[] translation = { TransformParameters[ 3 ], TransformParameters[ 4 ], TransformParameters[ 5 ]};
+        return translation;
+    }
+
+    public double[] getRotationCenterInMillimeters()
+    {
+        final double[] rotationCenter = { CenterOfRotationPoint[ 0 ], CenterOfRotationPoint[ 1 ], CenterOfRotationPoint[ 2 ]};
+        return rotationCenter;
+    }
 }
