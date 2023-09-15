6add4e17212982317e3bfe1f1f8dac1f91091cb0
John Bogovic <bogovicj@janelia.hhmi.org>
2023-09-15 09:03:06 +0200

    toward using imglib2-6, -realtransform-4

--- src/main/java/itc/transforms/imglib2/BSplineDisplacementField.java
+++ src/main/java/itc/transforms/imglib2/BSplineDisplacementField.java
@@ -2,6 +2,7 @@ package itc.transforms.imglib2;
 
 import java.util.List;
 
+import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.RealInterval;
 import net.imglib2.RealLocalizable;
@@ -9,9 +10,11 @@ import net.imglib2.RealPositionable;
 import net.imglib2.RealRandomAccess;
 import net.imglib2.RealRandomAccessible;
 import net.imglib2.algorithm.interpolation.randomaccess.BSplineCoefficientsInterpolatorFactory;
+import net.imglib2.interpolation.InterpolatorFactory;
+import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory;
 import net.imglib2.outofbounds.OutOfBoundsFactory;
 import net.imglib2.realtransform.AffineGet;
-import net.imglib2.realtransform.DeformationFieldTransform;
+import net.imglib2.realtransform.DisplacementFieldTransform;
 import net.imglib2.realtransform.RealTransform;
 import net.imglib2.realtransform.RealViews;
 import net.imglib2.realtransform.Scale;
@@ -20,6 +23,10 @@ import net.imglib2.realtransform.Translation;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.util.Util;
 import net.imglib2.view.Views;
+import net.imglib2.view.composite.Composite;
+import net.imglib2.view.composite.CompositeIntervalView;
+import net.imglib2.view.composite.RealComposite;
+import net.imglib2.view.composite.RealStackCompositeView;
 
 /**
  * A displacement field stored as BSpline coefficients.
@@ -38,7 +45,7 @@ public class BSplineDisplacementField< T extends RealType<T> > implements RealTr
 
 	private final double[] gridSpacing;
 
-	private final DeformationFieldTransform< T > dfield;
+	private final DisplacementFieldTransform dfield;
 
 	@SuppressWarnings( "unchecked" )
 	public BSplineDisplacementField( 
@@ -89,19 +96,18 @@ public class BSplineDisplacementField< T extends RealType<T> > implements RealTr
 			}
 		}
 
-		dfield = new DeformationFieldTransform<T>( defAccesses );
+		final RealRandomAccessible<RealComposite<T>> vectors = new RealStackCompositeView<T>( defAccesses );
+		dfield = new DisplacementFieldTransform( vectors );
 	}
 
 	public BSplineDisplacementField(
 			final int numDimensions,
-			DeformationFieldTransform<T> dfield )
+			final DisplacementFieldTransform dfield )
 	{
 		this.numDimensions = numDimensions;
 		this.gridOffset = null;
 		this.gridSpacing = null;
 		this.dfield = dfield;
-	}
-
 	@Override
 	public int numSourceDimensions()
 	{
@@ -130,7 +136,36 @@ public class BSplineDisplacementField< T extends RealType<T> > implements RealTr
 	@Override
 	public RealTransform copy()
 	{
-		return new BSplineDisplacementField<>( numDimensions, ( DeformationFieldTransform< T > ) dfield.copy() );
+		return new BSplineDisplacementField<>( numDimensions, ( DisplacementFieldTransform ) dfield.copy() );
 	}
+	
+	private static < T extends RealType< T > > RealRandomAccessible< ? extends RealLocalizable > convertToComposite(
+			final RandomAccessibleInterval< T > position )
+	{
+		final CompositeIntervalView< T, RealComposite< T > > collapsedFirst =
+				Views.collapseReal(
+						Views.moveAxis( position, 0, position.numDimensions() - 1 ) );
+
+		return Views.interpolate(
+				Views.extendBorder( collapsedFirst ),
+				new NLinearInterpolatorFactory<>() );
+	}
+	
+	public static class BSplineCompositeInterpolator<T extends RealType<T>, S extends Composite<T>> implements InterpolatorFactory< T, RandomAccessible< S > > {
+
+		@Override
+		public RealRandomAccess<T> create(RandomAccessible<S> f) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public RealRandomAccess<T> create(RandomAccessible<S> f, RealInterval interval) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+
 
 }
