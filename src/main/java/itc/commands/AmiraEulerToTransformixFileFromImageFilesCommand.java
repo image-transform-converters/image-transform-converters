0920aa339812a33b832d526efa31d67b0c3d0f0e
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2023-11-05 22:03:06 +0100

    remove useless imports

--- src/main/java/itc/commands/AmiraEulerToTransformixFileFromImageFilesCommand.java
+++ src/main/java/itc/commands/AmiraEulerToTransformixFileFromImageFilesCommand.java
@@ -28,25 +28,11 @@
  */
 package itc.commands;
 
-import itc.converters.AffineTransform3DToElastixAffine3D;
-import itc.converters.AmiraEulerToAffineTransform3D;
-import itc.converters.BigWarpAffineToElastixAffineTransform3D;
-import itc.transforms.elastix.ElastixAffineTransform3D;
-import itc.transforms.elastix.ElastixTransform;
-import itc.utilities.ParsingUtils;
-import loci.common.services.ServiceFactory;
-import loci.formats.IFormatReader;
-import loci.formats.ImageReader;
-import loci.formats.meta.IMetadata;
-import loci.formats.services.OMEXMLService;
-import net.imglib2.realtransform.AffineTransform3D;
-import ome.units.UNITS;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 import java.io.File;
-import java.util.Arrays;
 
 @Plugin(type = Command.class, menuPath = "Plugins>Registration>Elastix>Utils>Amira Euler Transform to Transformix File from Image Files" )
 public class AmiraEulerToTransformixFileFromImageFilesCommand extends AbstractAmiraEulerToTransformixFileCommand
